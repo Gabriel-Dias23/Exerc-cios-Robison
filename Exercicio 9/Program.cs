d189e2aa18cecbb0f145680aff27451bafcd77e2
gabriel dias <bieldias023@outlook.com>
2025-03-02 09:54:18 -0300

    COMIT FINAL

--- Exercicio 9/Program.cs
+++ Exercicio 9/Program.cs
@@ -1,29 +1,25 @@
 ﻿using System;
-using System.Linq;
 
 class Program
 {
     static void Main()
     {
-        double[] notas = new double[6]; // Vetor para armazenar as 6 notas dos juízes
+        string aranha = @"
+              (
+               )
+              (
+         /\  .-\""-\" -.  /\
+        //\\/  ,,,  \//\\
+        |/\| ,; ; ; ; ;, |/\|
+      //\\\;-\""-\"-;///\\
+      //  \\/   .   \//  \\
+      (| , -_ | \\ | / | _ -, |)
+       //`__\\.-.-./__`\\
+       //   /`   '   `\\   \\
+     / (_ /     \\   _)\ 
+      `-._ /       \\_.- '
+        ";
 
-        Console.WriteLine("Digite as 6 notas dos juízes (valores entre 0 e 10):");
-
-        // Leitura das notas
-        for (int i = 0; i < 6; i++)
-        {
-            Console.Write($"Nota {i + 1}: ");
-            notas[i] = double.Parse(Console.ReadLine());
-        }
-
-        // Encontrar a maior e a menor nota
-        double maior = notas.Max();
-        double menor = notas.Min();
-
-        // Calcular a soma excluindo a maior e a menor nota
-        double soma = notas.Sum() - maior - menor;
-
-        // Exibir o resultado final
-        Console.WriteLine($"\nNota final do atleta (excluindo maior e menor nota): {soma:F2}");
+        Console.WriteLine(aranha);
     }
 }
---
+++ Exercício 10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] A = new int[4, 4];
+
+    
+        Console.WriteLine("Digite os 16 elementos da matriz 4x4:");
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                A[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+  
+        int somaA = 0, somaB = 0, somaC = 0, somaD = 0;
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+             
+                if (i < 2 && j < 2)
+                    somaA += A[i, j];
+
+           
+                if (i >= 2 && j >= 2)
+                    somaB += A[i, j];
+l
+                if (i > j)
+                    somaC += A[i, j];
+
+               
+                if (i < j)
+                    somaD += A[i, j];
+            }
+        }
+
+      
+        Console.WriteLine($"Soma da região (a): {somaA}");
+        Console.WriteLine($"Soma da região (b): {somaB}");
+        Console.WriteLine($"Soma da região (c): {somaC}");
+        Console.WriteLine($"Soma da região (d): {somaD}");
+    }
+}
+
